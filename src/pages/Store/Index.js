a7a78e758f24a4e44fd18d6f17f8404927d3289f
maruro <31755063+nguyentu43@users.noreply.github.com>
2021-10-27 21:03:14 +0700

    add recommend to index

--- src/pages/Store/Index.js
+++ src/pages/Store/Index.js
@@ -4,13 +4,17 @@ import FavouriteAuthorBlock from '../../components/Store/Block/FavouriteAuthorBl
 import FeaturedBookBlock from '../../components/Store/Block/FeaturedBookBlock';
 import FeaturedCategoryBlock from '../../components/Store/Block/FeaturedCategoryBlock';
 import NewReleaseBlock from '../../components/Store/Block/NewReleaseBlock';
+import RecommendationBlock from '../../components/Store/Block/RecommendationBlock';
 import WeekDealBlock from '../../components/Store/Block/WeekDealBlock';
+import useAppContext from '../../context';
 
 export default function Index() {
+  const {state:{auth}} = useAppContext();
   return (
     <>
       <BannerCarousel />
       <FeaturedCategoryBlock />
+      { auth.isLogin && <RecommendationBlock/> }
       <BestSellingBlock />
       <FeaturedBookBlock />
       <WeekDealBlock />
